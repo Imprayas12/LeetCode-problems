ed0dcba66abf5fb10e285a1b138e9c9ebf16cd00
Prayas Gautam <prayas12gautam@gmail.com>
2022-01-21 13:04:47 +0530

    Time: 4 ms (31.00%), Space: 63.7 MB (39.94%) - LeetHub

---
+++ 134-gas-station/134-gas-station.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int index=0;
+        int currGas=0;
+        int remGas=0;
+        for(int i=0;i<gas.length;i++){
+            currGas += gas[i]-cost[i];
+            if(currGas<0){
+                index=i+1;
+                remGas+=currGas;
+                currGas=0;
+            }
+        }
+        return (remGas+currGas>=0) ? index : -1;
+    }
+}
