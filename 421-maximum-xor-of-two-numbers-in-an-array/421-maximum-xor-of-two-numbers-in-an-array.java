59cb06f38a8f2d005ceb7023337ffc0fe599f9f8
Prayas Gautam <prayas12gautam@gmail.com>
2022-01-27 21:56:18 +0530

    Time: 29 ms (98.32%), Space: 86.3 MB (51.10%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,19 +1,22 @@
 class Solution {
     public int findMaximumXOR(int[] nums) {
-        int mask=0;
-        int max=0;
-        for(int i=31;i>=0;i--){
-            mask = mask|1<<i;
-            Set<Integer> st = new HashSet<>();
-            for(var num : nums){
-                st.add(num & mask);
-            }
-            int temp = max | (1<<i);
-            for(int num : st){
-                if(st.contains(temp^num)){
-                    max = temp;
+        int max=0,mask=0;
+        HashSet<Integer> set=new HashSet<Integer>();
+        int maxNum=0;
+        for(int i:nums)
+            maxNum=Math.max(maxNum,i);
+        for(int i=31-Integer.numberOfLeadingZeros(maxNum);i>=0;i--){
+            set.clear();
+            int bit=1<<i;
+            mask=mask|bit;
+            int temp=max|bit;
+            for(int prefix:nums){
+                if(set.contains((prefix & mask)^temp))
+                { 
+                    max=temp;
                     break;
                 }
+                set.add(prefix & mask);
             }
         }
         return max;
