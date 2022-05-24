95fc4db592cfaff2d3da8fe25e7bf45a7b3c5635
Prayas Gautam <prayas12gautam@gmail.com>
2022-05-24 21:43:11 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (20.54%) - LeetHub

--- 492-construct-the-rectangle/492-construct-the-rectangle.java
+++ 492-construct-the-rectangle/492-construct-the-rectangle.java
@@ -1,17 +1,15 @@
 import java.util.*;
 class Solution {
    public static int[] constructRectangle(int area) {
-        if (area == 0)
+        if (area == 0)// Edge Case
             return new int[]{0};
-
-        return constructRectangle(area, (int) Math.sqrt(area));
+        return constructRectangle(area,(int)Math.sqrt(area)); 
+       //Calling helper method, passing width as max possible width.
     }
 
-    private static int[] constructRectangle(int area, int l) {
-
-        if (area % l == 0)
-            return new int[]{(area / l), l};
-
-        return constructRectangle(area, l - 1);
+    private static int[] constructRectangle(int area, int w) {
+        if (area % w == 0) //Wherever current width is a factor, difference b/w length and current width will be minimum.
+            return new int[]{(area / w), w};
+        return constructRectangle(area, w - 1); //Decreasing width by 1.
     }
 }
