ab77088d35d59f4b1c54966fc1b8349e7d27bddf
Prayas Gautam <prayas12gautam@gmail.com>
2022-05-31 23:11:00 +0530

    Time: 3 ms (39.60%), Space: 43.8 MB (61.02%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        findSubs(0,nums,ans,new ArrayList<>());
+        return ans;
+    }
+    private void findSubs(int index,int[] arr,List<List<Integer>> ans  , List<Integer> ds){
+        for(int i=index;i<arr.length;i++){
+            if(i==index || arr[i]!=arr[i-1]) {
+            ds.add(arr[i]);
+            findSubs(i+1,arr,ans,ds);
+            ds.remove(ds.size()-1);
+            }
+        }
+        ans.add(new ArrayList<>(ds));
+    }
+}
