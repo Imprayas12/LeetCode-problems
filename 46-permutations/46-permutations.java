a15e9944468c0e9e79afbb0da374cddc260e8897
Prayas Gautam <prayas12gautam@gmail.com>
2022-05-31 23:56:49 +0530

    Time: 3 ms (37.27%), Space: 44.9 MB (36.44%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        set(nums,ans,new ArrayList<Integer>(),new HashMap<Integer,Boolean>());
+        set(nums,0,ans);
         return ans;
     }
-    private void set(int[] arr,List<List<Integer>> ans,List<Integer> ds,HashMap<Integer,Boolean> map){
-        if(ds.size()==arr.length){
+    private void set(int[] arr,int index,List<List<Integer>> ans){
+        if(index==arr.length){
+            List<Integer> ds = new ArrayList<Integer>();
+            for(int i=0;i<arr.length;i++) ds.add(arr[i]);
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=0;i<arr.length;i++){
-            if(!map.containsKey(arr[i])){
-                ds.add(arr[i]);
-                map.put(arr[i],true);
-                set(arr,ans,ds,map);
-                ds.remove(ds.size()-1);
-                map.remove(arr[i]);
-            }
+        for(int i=index;i<arr.length;i++){
+            swap(arr,index,i);
+            set(arr,index+1,ans);
+            swap(arr,index,i);
         }
+    }
+    private void swap(int[] arr,int i,int j){
+        int a = arr[i];
+        arr[i]=arr[j];
+        arr[j]=a;
     }
 }
