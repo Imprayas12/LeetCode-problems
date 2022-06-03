b75ff6acc3d7a0a5f72ce86deceb209cc6f5b727
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-03 23:28:10 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (45.56%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,25 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
+        int n = nums.length;
         if(nums.length==1) return nums[0];
-        return Math.max(rob0(nums),rob1(nums));
+        if(nums.length==2) return Math.max(nums[0],nums[1]);
+        int[] dp1 = new int[nums.length-1];
+        int[] dp2 = new int[nums.length-1];
+        for(int i=0;i<n;i++){
+            if(i!=0) dp1[i-1] = nums[i];
+            if(i!=n-1) dp2[i] = nums[i];
+        }
+        return Math.max(getMaxAmount(dp1),getMaxAmount(dp2));
     }
-     public int rob0(int[] nums){
-      int preMax = 0, curMax = 0;
-      for(int i = 0; i < nums.length - 1; i++){
-        int t = curMax;
-        curMax = Math.max(preMax + nums[i], curMax);
-        preMax = t;  
-      }  
-      return curMax;
-    }
-    
-    public int rob1(int[] nums){
-      int preMax = 0, curMax = 0;
-      for(int i = 1; i < nums.length; i++){
-        int t = curMax;
-        curMax = Math.max(preMax + nums[i], curMax);
-        preMax = t;  
-      }  
-      return curMax;  
+    private int getMaxAmount(int[] arr){
+        int n = arr.length;
+        int prev = arr[0];
+        int prev2 = 0;
+        int max = 0;
+        for(int i=1;i<n;i++){
+            int take = arr[i];
+            int not_take = -1;
+            if(i>1) take += prev2;
+                not_take = prev;
+            max = Math.max(take,not_take);
+            prev2 = prev;
+            prev = max;
+        }
+        return max;
     }
 }
