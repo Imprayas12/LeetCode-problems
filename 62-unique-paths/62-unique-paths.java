131a131bfdd11c83bc473f28360d09507537967c
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-04 23:21:04 +0530

    Time: 1 ms (42.33%), Space: 40.6 MB (62.22%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return countWays(m-1,n-1,dp);
+        return countWays(m-1,n-1,dp,m,n);
     }
-    int countWays(int row,int col,int[][] dp){
-        if(row==0 && col==0){
-            return 1;
+    int countWays(int row,int col,int[][] dp,int m,int n){
+        dp[0][0] = 1;
+        
+        for(row=0;row<m;row++){
+            for(col=0;col<n;col++){
+            if(row==0 && col==0){
+              dp[0][0]=1; 
+                continue;
+            }  
+            int down = 0;
+            int right = 0;
+             if(row>0)   down = dp[row-1][col];
+             if(col>0)   right = dp[row][col-1];      
+                dp[row][col] = down+right;    
+            }
         }
-        if(row<0 || col<0)
-            return 0;
-        if(dp[row][col]!=0) return dp[row][col];
-        int up = countWays(row-1,col,dp);
-        int left = countWays(row,col-1,dp);
-        return dp[row][col] = up + left;
+        return dp[m-1][n-1];
     }
 }
