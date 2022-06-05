a9aa7e33f63a7b8c7622d7ebf84d4f9a87528ae6
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-05 13:46:09 +0530

    Time: 10 ms (17.02%), Space: 43.5 MB (68.07%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -3,15 +3,20 @@ class Solution {
         int n = triangle.size();
         int m = triangle.get(n-1).size();
         int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++) for(int j=0;j<m;j++) dp[i][j]=-1;
         return minPath(triangle,0,0,dp);
     }
-    private int minPath(List<List<Integer>> arr,int i,int j,int[][] dp){
-        if(i==arr.size()-1) return arr.get(i).get(j);
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = arr.get(i).get(j) + minPath(arr,i+1,j,dp);
-        int down_right = arr.get(i).get(j) + minPath(arr,i+1,j+1,dp);
-        
-        return dp[i][j]=Math.min(down,down_right);
+    private int minPath(List<List<Integer>> arr,int k,int l,int[][] dp){
+        int n = arr.size()-1;
+        for(int i=0;i<arr.get(n).size();i++){
+            dp[n][i]=arr.get(n).get(i);
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+            int down = arr.get(i).get(j) + dp[i+1][j];
+            int down_right = arr.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(down,down_right);
+            }
+        }
+        return dp[0][0];
     }
 }
