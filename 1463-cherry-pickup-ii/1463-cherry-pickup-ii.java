cac6b1ed05a9f45cbb3655604f4df2b2efb0b939
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-06 23:38:09 +0530

    Time: 83 ms (6.93%), Space: 46.5 MB (60.46%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -5,28 +5,41 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int[][][] dp = new int[n][m][m];
-        return maxCherry(0,0,grid[0].length-1,grid,di,dj,dp);
+        return maxCherry(grid,di,dj,dp);
     }
-    private int maxCherry(int i,int j1,int j2,int[][] grid,int[] di,int[] dj,int[][][] dp){
-        
-        if(j1<0 || j1>grid[0].length-1 || j2<0 || j2>grid[0].length-1)
-            return 0;
-        if(i==grid.length-1){
-            if(j1==j2)
-                return grid[i][j1];
-            else{
-                return grid[i][j1]+grid[i][j2];
+    private int maxCherry(int[][] grid,int[] di,int[] dj,int[][][] dp){
+        int n = grid.length;
+        for(int k=0;k<dp[0].length;k++){
+            for(int l=0;l<dp[0].length;l++){
+                if(k==l)
+                dp[n-1][k][l]=grid[n-1][k];
+                else
+                dp[n-1][k][l]=grid[n-1][k]+grid[n-1][l];
             }
         }
-        if(dp[i][j1][j2]!=0) return dp[i][j1][j2]; 
-        int ans = 0;
-        for(var k: di){
-            for(var l: dj){
-                if(j1==j2) 
-                    ans = Math.max(ans,grid[i][j1]+maxCherry(i+1,j1+k,j2+l,grid,di,dj,dp));
-                else ans = Math.max(ans,grid[i][j1]+grid[i][j2] +maxCherry(i+1,j1+k,j2+l,grid,di,dj,dp));
+        int m = grid[0].length;
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int ansi = Integer.MIN_VALUE;
+                    for(int k:di){
+                        for(int l:dj){
+                            int ans=0;
+                            if(j1==j2)
+                                ans = grid[i][j1];
+                            else ans = grid[i][j1]+grid[i][j2];
+                            if(j1+k>=0 && j2+l>=0 && j1+k<m && j2+l<m){
+                            ans += dp[i+1][j1+k][j2+l];
+                                ansi = Math.max(ans,ansi);
+                            }
+                            else
+                            ans += -100000000;
+                        }
+                    }
+                    dp[i][j1][j2]= ansi;
+                }
             }
         }
-        return dp[i][j1][j2] = ans;
+        return dp[0][0][m-1];
     }
 }
