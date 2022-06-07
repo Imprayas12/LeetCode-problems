69038acd375ab22829e2ae68e2fd05b07e4c3e60
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-07 22:22:30 +0530

    Time: 20 ms (77.57%), Space: 51.5 MB (74.00%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.java
+++ 741-cherry-pickup/741-cherry-pickup.java
@@ -1,32 +1,21 @@
 class Solution {
-    int[][][] memo;
-    int[][] grid;
-    int N;
     public int cherryPickup(int[][] grid) {
-        this.grid = grid;
-        N = grid.length;
-        memo = new int[N][N][N];
-        for (int[][] layer: memo)
-            for (int[] row: layer)
-                Arrays.fill(row, Integer.MIN_VALUE);
-        return Math.max(0, dp(0, 0, 0));
+        int n = grid.length;
+        int[][][] dp = new int[n][n][n];
+        for(int i=0;i<n;i++) for(int j=0;j<n;j++) for(int k=0;k<n;k++)
+            dp[i][j][k] = Integer.MIN_VALUE;
+     return Math.max(0,cherryCount(grid,0,0,0,grid.length,dp));   
     }
-    public int dp(int r1, int c1, int c2) {
-        int r2 = r1 + c1 - c2;
-        if (N == r1 || N == r2 || N == c1 || N == c2 ||
-                grid[r1][c1] == -1 || grid[r2][c2] == -1) {
-            return -999999;        
-        } else if (r1 == N-1 && c1 == N-1) {
-            return grid[r1][c1];
-        } else if (memo[r1][c1][c2] != Integer.MIN_VALUE) {
-            return memo[r1][c1][c2];
-        } else {
-            int ans = grid[r1][c1];
-            if (c1 != c2) ans += grid[r2][c2];
-            ans += Math.max(Math.max(dp(r1, c1+1, c2+1), dp(r1+1, c1, c2+1)),
-                            Math.max(dp(r1, c1+1, c2), dp(r1+1, c1, c2)));
-            memo[r1][c1][c2] = ans;
-            return ans;
-        }
+    private int cherryCount(int[][] grid,int i,int j1,int j2,int n,int[][][] dp){
+        int i2 = i + j1-j2;
+        if(i==n || i2==n || j1==n || j2==n || grid[i][j1]==-1 || grid[i2][j2]==-1 ) return Integer.MIN_VALUE;
+        else if(i==n-1 && j1==n-1) return grid[i][j1];
+        else if(dp[i][j1][j2]!= Integer.MIN_VALUE) return dp[i][j1][j2];
+        else {
+        int ans = grid[i][j1];
+        if(j1!=j2) ans+= grid[i2][j2];
+        ans += Math.max(Math.max(cherryCount(grid,i,j1+1,j2+1,n,dp),cherryCount(grid,i+1,j1,j2+1,n,dp)), Math.max(cherryCount(grid,i,j1+1,j2,n,dp),cherryCount(grid,i+1,j1,j2,n,dp)));
+        return dp[i][j1][j2]=ans;
+             }
     }
 }
