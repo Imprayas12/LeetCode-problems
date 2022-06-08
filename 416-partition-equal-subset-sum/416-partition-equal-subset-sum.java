8d87a4e12ffb21a8c5d1cb06586461826c27f2ee
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-08 23:40:55 +0530

    Time: 84 ms (31.64%), Space: 53.5 MB (61.09%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -4,22 +4,18 @@ class Solution {
     int sum=0;
         for(var i:nums) sum+=i;
         if(sum%2==1) return false;
-        int[][] dp = new int[n][sum/2+1];
-        for(var i: dp){
-            Arrays.fill(i,-1);
+        boolean[][] dp = new boolean[n][sum/2+1];
+        int k = sum/2;
+        for(int i=0;i<n;i++) dp[i][0]= true;
+        if(nums[0]<=k) dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=k;j++){
+                boolean notTake = dp[i-1][j];
+                boolean take = false;
+                if(nums[i]<=j) take = dp[i-1][j-nums[i]];
+                dp[i][j]=take||notTake;
+            }
         }
-        return f(nums,n-1,sum/2,dp);
-    }
-    private boolean f(int[] nums,int index,int target,int[][] dp){
-        if(target==0)
-            return true;
-        if(index==0) 
-            return nums[0]==target;
-        if(dp[index][target]!=-1) return dp[index][target]==1?true:false;
-        boolean notTake = f(nums,index-1,target,dp);
-        boolean take = false;
-        if(nums[index]<=target) take = f(nums,index-1,target-nums[index],dp);
-        dp[index][target]=take||notTake?1:0;
-        return  take||notTake;
+        return dp[n-1][k];
     }
 }
