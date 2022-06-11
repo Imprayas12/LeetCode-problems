1100c5162aef6c4f614ae98314c331bbb5eac2ea
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-11 21:51:00 +0530

    Time: 39 ms (32.80%), Space: 53.9 MB (19.88%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,10 +3,21 @@ class Solution {
         if(coins.length==1)
             return amount%coins[0]==0?amount/coins[0]:-1;
         int[][] dp = new int[coins.length][amount+1];
-        for(int[] i:dp)
-            Arrays.fill(i,999999);
-        int ans = coinCount(coins.length-1,coins,amount,dp);
-        return ans==9999999?-1:ans;
+       for(int i=0;i<=amount;i++){
+           if(i%coins[0]==0)
+               dp[0][i]=i/coins[0];
+           else
+               dp[0][i]=999999;
+       }
+        for(int ind=1;ind<coins.length;ind++){
+            for(int amt=0;amt<=amount;amt++){
+             int notTake = dp[ind-1][amt];
+            int take = Integer.MAX_VALUE;
+            if(coins[ind]<=amt) take = 1+ dp[ind][amt-coins[ind]];   
+            dp[ind][amt]=Math.min(take,notTake);
+            }
+        }
+        return dp[coins.length-1][amount]==999999?-1:dp[coins.length-1][amount];
     }
     private int coinCount(int ind,int[] coins,int amt,int[][] dp){
         if(ind==0){
