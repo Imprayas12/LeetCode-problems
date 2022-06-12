428af78e36cc5a1244faa267036ccd91a67379f7
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-12 23:43:58 +0530

    Time: 18 ms (24.27%), Space: 50.7 MB (35.97%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,16 +1,17 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length][amount+1];
-        for(int[] i: dp) Arrays.fill(i,-1);
-        return count(coins.length - 1,amount,coins,dp);
-    }
-    private int count(int ind,int amt,int[] coins,int[][] dp){
-        if(amt == 0) return 1;
-        if(ind < 0) return 0;
-        if(dp[ind][amt] != -1) return dp[ind][amt];
-        int notTake = count(ind - 1, amt, coins,dp);
-        int take = 0;
-        if(coins[ind] <= amt) take = count(ind,amt - coins[ind],coins,dp);
-        return dp[ind][amt] = take + notTake;
+        for(int i=0;i<=amount;i++){
+           if(i % coins[0] == 0) dp[0][i] = 1;
+        }
+        for(int ind = 1;ind < coins.length;ind++){
+            for(int amt = 0;amt <= amount; amt++){
+                int notTake = dp[ind-1][amt];
+                int take = 0;
+                if(coins[ind]<=amt) take = dp[ind][amt-coins[ind]];
+                dp[ind][amt] = take + notTake;
+            }
+        }
+        return dp[coins.length-1][amount];
     }
 }
