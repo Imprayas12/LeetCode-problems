1424fa3a4504f676fcf1ad4bf7183cc125b74b52
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-17 12:18:08 +0530

    Time: 44 ms (12.14%), Space: 50.8 MB (12.18%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,19 +1,21 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()][t.length()];
-        for(var i: dp) Arrays.fill(i, -1);
+        int[][] dp = new int[s.length()+1][t.length()+1];
         return disSubs(s.length()-1,t.length()-1,s,t,dp);
     }
     private int disSubs(int ind1,int ind2,String s,String t,int[][] dp){
         
-         if(ind2<0) return 1; 
-        if(ind1<0) return 0;
-       if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s.charAt(ind1)==t.charAt(ind2)){
-            return dp[ind1][ind2] = disSubs(ind1-1,ind2-1,s,t,dp)+disSubs(ind1-1,ind2,s,t,dp);
-        }
-        else{
-            return  dp[ind1][ind2] =  disSubs(ind1-1,ind2,s,t,dp);
-        }
+       for(int i=0;i<=s.length();i++){
+           dp[i][0] = 1;
+       }
+       for(int i=1;i<=s.length();i++){
+           for(int j=1;j<=t.length();j++){
+               if(s.charAt(i-1)==t.charAt(j-1))
+                   dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+               else
+                   dp[i][j] = dp[i-1][j];
+           }
+       }
+        return dp[s.length()][t.length()];
     }
 }
