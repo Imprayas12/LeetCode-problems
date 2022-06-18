acf9084c7409459dcb38b048fba64009b825edb5
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-18 22:54:04 +0530

    Time: 4 ms (9.76%), Space: 44.8 MB (15.88%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-       for(var i: dp) Arrays.fill(i,Integer.MIN_VALUE);
-        return max(0,1,prices,dp);
+        int[][] dp = new int[prices.length+1][2];
+        dp[prices.length][0] = dp[prices.length][1] = 0;
+        for(int i = prices.length-1;i>=0;i--){
+            for(int buy = 0;buy<=1;buy++){
+                int profit = 0;
+                if(buy == 1)
+                    profit = Math.max(-prices[i] + dp[i+1][0],dp[i+1][1]);
+                else
+                    profit = Math.max(prices[i] + dp[i+1][1],dp[i+1][0]);
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
     private int max(int ind,int canBuy,int[] prices,int[][] dp){
         if(ind == prices.length)
             return 0;
-        if(dp[ind][canBuy]!= Integer.MIN_VALUE) return dp[ind][canBuy];
+        if(dp[ind][canBuy] != Integer.MIN_VALUE) return dp[ind][canBuy];
         int profit = 0;
         if(canBuy==1){
             profit = Math.max(-prices[ind]+max(ind+1,0,prices,dp),0+max(ind+1,1,prices,dp));
