9a3ea86b9391f423d53cddcf4c32dba7800f0762
Prayas Gautam <prayas12gautam@gmail.com>
2022-06-24 10:58:28 +0530

    Time: 172 ms (9.72%), Space: 118 MB (6.50%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,12 +1,14 @@
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
-    public boolean isMatch(String s, String p) {
-        Pattern pattern = Pattern.compile("("+p+")");
-        Matcher match = pattern.matcher(s);
-        if(match.find() && match.group(0)==s)return true;
-        return false;
+    public boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() &&
+                               (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
     }
 }
