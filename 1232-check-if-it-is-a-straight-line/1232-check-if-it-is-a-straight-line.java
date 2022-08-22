9833cfb5ec1af055cbe7cc1f8405758cee91acc2
Prayas Gautam <prayas12gautam@gmail.com>
2022-08-22 08:45:18 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (15.91%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -2,12 +2,7 @@ class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
         if(coordinates.length == 2) return true;
         for(int i = 1; i < coordinates.length - 1; i++){
-            double nr1 = ((double)coordinates[i][1] - (double)coordinates[i-1][1]);
-            double dr1 = ((double)coordinates[i][0] - (double)coordinates[i-1][0]);
-            
-            double nr2 = ((double)coordinates[i + 1][1] - (double)coordinates[i][1]);
-            double dr2 = ((double)coordinates[i + 1][0] - (double)coordinates[i][0]);
-            if(nr1 * dr2 != nr2 * dr1) return false;
+            if(((double)coordinates[i][1] - (double)coordinates[i-1][1]) * ((double)coordinates[i + 1][0] - (double)coordinates[i][0]) !=  ((double)coordinates[i + 1][1] - (double)coordinates[i][1]) * ((double)coordinates[i][0] - (double)coordinates[i-1][0])) return false;
         }
         return true;
     }
