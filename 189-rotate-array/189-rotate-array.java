940155531ede90239fb67c82e7ae42389cddd4e1
Prayas Gautam <prayas12gautam@gmail.com>
2022-08-25 15:36:17 +0530

    Time: 3 ms (26.81%), Space: 64.1 MB (70.27%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
      /*For array to rotate k times... the lowest element of the array must be at kth index after rotations.*/
-        k%=nums.length;
-        if(nums.length==0||nums.length==1) return;
+        k %= nums.length;
+        if(nums.length==0 || nums.length==1 || k == 0) return;
         int[] newnum = new int[nums.length];
-        int j=0;
-        for(int i=k;i<newnum.length;i++){
-            newnum[i]=nums[j++];
-        }
-        for(int i=0;i<k;i++){
-            newnum[i]=nums[j++];
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=newnum[i];
+        int j = 0;
+        reverse(nums,0,nums.length - 1);
+        reverse(nums,0, k - 1);
+        reverse(nums,k ,nums.length - 1);
+    }
+    private void reverse(int[] nums,int i,int j){ 
+        while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
