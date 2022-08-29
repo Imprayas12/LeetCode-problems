36a2d07c06836b7793d4d496ac66d31253778cb9
Prayas Gautam <prayas12gautam@gmail.com>
2022-08-29 23:28:15 +0530

    Time: 4 ms (76.67%), Space: 57.9 MB (27.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,50 +1,26 @@
 class Solution {
-    class Pair {
-        int x;
-        int y;
-        Pair(int x,int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] vis = new int[m][n];
+        // int[][] vis = new int[m][n];
         int ans = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(vis[i][j] == 0 && grid[i][j] == '1'){
-                    bfs(i,j,vis,grid);
+                if(grid[i][j] == '1'){
+                    bfs(i,j,grid);
                     ans++;
                 }
             }
         }
         return ans;
     }
-    private void bfs(int row,int col, int[][] vis, char[][] grid) {
-        vis[row][col] = 1;
-        Queue<Pair> queue = new LinkedList<Pair>();
-        queue.add(new Pair(row,col));
-        int m = grid.length;
-        int n = grid[0].length;
-        while(!queue.isEmpty()) {
-            int r = queue.peek().x;
-            int c = queue.peek().y;
-            queue.poll();
-            for(int dr = -1; dr <= 1; dr++){
-                for(int dc = -1; dc <= 1; dc++){
-                    
-                      if((dr == -1 && dc == -1) || (dr == -1 && dc == 1) || (dr == 1 && dc == -1) || (dr == 1 && dc == 1)){
-                        continue;
-                    }
-                    int rowC = r + dr;
-                    int colD = c + dc;
-                    if(rowC >= 0 && rowC < m && colD >= 0 && colD < n && grid[rowC][colD] == '1' && vis[rowC][colD] != 1){
-                        bfs(rowC,colD,vis,grid);
-                    }
-                }
-            }
+    private void bfs(int i,int j,char[][] grid) {
+        if(i >= 0 && i < grid.length && j >=0 && j < grid[0].length && grid[i][j] != '0'){
+            grid[i][j] = '0';
+            bfs(i - 1, j, grid);
+            bfs(i ,j - 1, grid);
+            bfs(i + 1, j, grid);
+            bfs(i, j + 1, grid);
         }
     }
 }
