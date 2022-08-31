492723d2cdb556bb458165c2fa58529a923db77b
Prayas Gautam <prayas12gautam@gmail.com>
2022-08-31 14:46:47 +0530

    Time: 1 ms (92.12%), Space: 43.8 MB (51.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,11 +1,11 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums.length<=1 || nums==null) return;
+        if(nums.length <= 1 || nums == null) return;
         int i = nums.length-2;
-        while(i>=0 && nums[i]>=nums[i+1]) i--;
-        if(i>=0){
-            int j=nums.length-1;
-            while(nums[j]<=nums[i])j--;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        if(i >= 0){
+            int j = nums.length-1;
+            while(nums[j] <= nums[i])j--;
             swap(nums,i,j);
         }
         reverse(nums,i+1,nums.length-1);
