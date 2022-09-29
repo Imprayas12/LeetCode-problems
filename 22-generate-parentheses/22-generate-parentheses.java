98fddf409e9a6928ee6c1c9b49e2031ff7372570
Prayas Gautam <prayas12gautam@gmail.com>
2022-09-29 13:33:25 +0530

    Time: 3 ms (49.98%), Space: 44.9 MB (12.08%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        helper(ans,n,0,0,new StringBuilder());
+        helper(ans,0,0,n,"");
         return ans;
     }
-    private void helper(List<String> ans,int n,int open_par,int close_par, StringBuilder str){
-        if(str.length()==n*2){
-            ans.add(str.toString());
+    private void helper(List<String> ans,int open_par,int close_par,int n,String str) {
+        if(str.length() == n * 2) {
+            ans.add(str);
             return;
-        }
-        if(open_par<n){
-            str.append("(");
-            helper(ans,n,open_par+1,close_par,str);
-            str.deleteCharAt(str.length()-1);
-        }
-        if(close_par < open_par){
-            str.append(")");
-            helper(ans,n,open_par,close_par+1,str);
-            str.deleteCharAt(str.length()-1);
+        }       
+        if(open_par < n) {
+            str += '(';
+            helper(ans,open_par + 1,close_par,n,str);
+            str = str.substring(0,str.length() - 1);
+        }   
+        if(close_par < open_par) {
+            str += ')';
+            helper(ans,open_par,close_par + 1,n,str);
+            str = str.substring(0,str.length() - 1);
         }
     }
 }
