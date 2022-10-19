27af0738e1bdda5f4f1dbf02695f4a4d633a225c
Prayas Gautam <prayas12gautam@gmail.com>
2022-10-19 14:58:41 +0530

    Time: 129 ms (84.85%), Space: 40.5 MB (90.70%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        boolean ans = false;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == word.charAt(0))
+                ans = ans || check(board,word,i,j,new boolean[board.length][board[0].length],0);
+            }
+        } 
+        return ans;
+    }   
+    private boolean check(char[][] board,String word,int x,int y,boolean[][] vis,int ind) {
+        if(ind == word.length()) return true;
+        if(x < 0 || y < 0 || x >= board.length || y >= board[0].length || vis[x][y] || board[x][y] != word.charAt(ind)) return false;
+        vis[x][y] = true;
+        if(check(board,word,x + 1,y,vis,ind + 1)) return true;
+        if(check(board,word,x,y + 1,vis,ind + 1)) return true;
+        if(check(board,word,x - 1,y,vis,ind + 1)) return true;
+        if(check(board,word,x,y - 1,vis,ind + 1)) return true;
+        vis[x][y] = false;
+        return false;
+    }
+}
