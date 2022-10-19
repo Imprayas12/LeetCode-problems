15ba3ce9aebacfa5a306c5506f17c3725da1288f
Prayas Gautam <prayas12gautam@gmail.com>
2022-10-19 14:59:48 +0530

    Time: 1682 ms (5.01%), Space: 43 MB (19.90%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -4,19 +4,19 @@ class Solution {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++) {
                 if(board[i][j] == word.charAt(0))
-                ans = ans || check(board,word,i,j,new boolean[board.length][board[0].length],0);
+                ans = ans || check(board,word,i,j,new boolean[board.length][board[0].length],"",0);
             }
         } 
         return ans;
     }   
-    private boolean check(char[][] board,String word,int x,int y,boolean[][] vis,int ind) {
+    private boolean check(char[][] board,String word,int x,int y,boolean[][] vis,String cur,int ind) {
         if(ind == word.length()) return true;
         if(x < 0 || y < 0 || x >= board.length || y >= board[0].length || vis[x][y] || board[x][y] != word.charAt(ind)) return false;
         vis[x][y] = true;
-        if(check(board,word,x + 1,y,vis,ind + 1)) return true;
-        if(check(board,word,x,y + 1,vis,ind + 1)) return true;
-        if(check(board,word,x - 1,y,vis,ind + 1)) return true;
-        if(check(board,word,x,y - 1,vis,ind + 1)) return true;
+        if(check(board,word,x + 1,y,vis,cur + board[x][y],ind + 1)) return true;
+        if(check(board,word,x,y + 1,vis,cur + board[x][y],ind + 1)) return true;
+        if(check(board,word,x - 1,y,vis,cur + board[x][y],ind + 1)) return true;
+        if(check(board,word,x,y - 1,vis,cur + board[x][y],ind + 1)) return true;
         vis[x][y] = false;
         return false;
     }
