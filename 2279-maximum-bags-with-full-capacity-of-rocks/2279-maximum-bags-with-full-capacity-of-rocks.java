98d4ca5d629acfb35e0be47463d2611a684a644d
Prayas Gautam <prayas12gautam@gmail.com>
2022-12-27 11:28:35 +0530

    Time: 57 ms (28.61%), Space: 52.4 MB (88.50%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,30 +1,21 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        Integer[] diff = new Integer[capacity.length];
+        Integer[] difference = new Integer[capacity.length];
         for(int i = 0; i < capacity.length; i++) {
-            diff[i] = capacity[i] - rocks[i];
+            difference[i] = capacity[i] - rocks[i];
         }
-        Arrays.sort(diff);
+        Arrays.sort(difference);
         int ans = 0;
-        // for(int i = 0; i < diff.length; i++) {
-        //     System.out.println(additionalRocks + " " + diff[i]);
-        //     if(additionalRocks - diff[i] >= 0) {
-        //         diff[i] = 0;
-        //         additionalRocks = additionalRocks - diff[i];
-        //     }
-        //     if(diff[i] == 0) ans += 1;
-        // }
         int k = 0;
-        while(additionalRocks > 0 && k < diff.length) {
-            if(additionalRocks - diff[k] >= 0)
-            {
-                additionalRocks -= diff[k];
-                diff[k] = 0;
+        while(additionalRocks > 0 && k < difference.length) {
+            if(additionalRocks == 0) break;
+            if(additionalRocks - difference[k] >= 0){
+                additionalRocks -= difference[k];
+                difference[k] = 0;
             }  
             k++;
         }
-        // System.out.println(Arrays.asList(diff));
-        for(int i: diff) {
+        for(int i: difference) {
             if(i == 0) ans++;
         }
 
