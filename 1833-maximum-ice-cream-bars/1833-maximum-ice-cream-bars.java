8581a15de2deae96cc6c0c0edad690ae257a9345
Prayas Gautam <prayas12gautam@gmail.com>
2023-01-06 21:43:59 +0530

    Time: 13 ms (98.34%), Space: 79.3 MB (14.69%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,15 +1,36 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int idx = 0;
-        int ans = 0;
-        while(coins > 0 && idx < costs.length) {
-            if(costs[idx] <= coins) {
-                coins -= costs[idx];
-                ans++;
-            }
-            idx++;
+        int n = costs.length;
+        int icecreams = 0;
+
+        int m = costs[0];
+        for (int cost : costs) {
+            m = Math.max(m, cost);
         }
-        return ans;
+
+        int[] costsFrequency = new int[m + 1];
+        for (int cost : costs) {
+            costsFrequency[cost]++;
+        }
+
+        for (int cost = 1; cost <= m; ++cost) {
+            // No ice cream is present costing 'cost'.
+            if (costsFrequency[cost] == 0) {
+                continue;
+            }
+            // We don't have enough 'coins' to even pick one ice cream.
+            if (coins < cost) {
+                break;
+            }
+            
+            // Count how many icecreams of 'cost' we can pick with our 'coins'.
+            // Either we can pick all ice creams of 'cost' or we will be limited by remaining 'coins'.
+            int count = Math.min(costsFrequency[cost], coins / cost);
+            // We reduce price of picked ice creams from our coins.
+            coins -= cost * count;
+            icecreams += count;
+        }
+
+        return icecreams;
     }
 }
