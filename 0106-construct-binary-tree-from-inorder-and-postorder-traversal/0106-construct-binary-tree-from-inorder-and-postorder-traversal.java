c3a6c652252226854f2e74922438843ff4c6e4e5
Prayas Gautam <prayas12gautam@gmail.com>
2023-01-26 19:32:24 +0530

    Time: 4 ms (43.79%), Space: 45.4 MB (5.13%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,22 +14,21 @@
  * }
  */
 class Solution {
+    Map<Integer,Integer> map;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i);
+        }
         return createTree(inorder,0,inorder.length - 1, postorder, 0, postorder.length - 1);   
     }
     private TreeNode createTree(int[] inorder,int inLow,int inHigh,int[] postorder,int poLow,int poHigh) {
         if(inLow > inHigh || poLow > poHigh) return null;
         TreeNode newNode = new TreeNode(postorder[poHigh]);
-        int index = linearSearch(inorder,inLow,inHigh,postorder[poHigh]);
+        int index = map.get(postorder[poHigh]);
         int netElements = index - inLow;
         newNode.left = createTree(inorder,inLow,index - 1,postorder,poLow,poLow + netElements - 1);
         newNode.right = createTree(inorder,index + 1,inHigh,postorder,poLow + netElements,poHigh - 1);
         return newNode;
-    }
-    private int linearSearch(int[] arr,int start,int end,int item) {
-        for(int i = start; i <= end; i++) {
-            if(arr[i] == item) return i;
-        }
-        return -1;
     }
 }
