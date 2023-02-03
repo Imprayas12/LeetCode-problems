814688aaecd54652fcfee2b87e6c3b2fc69fb702
Prayas Gautam <prayas12gautam@gmail.com>
2023-02-03 16:05:54 +0530

    Time: 60 ms (16.31%), Space: 45.4 MB (29.03%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+        String ans = "";
+        int n = s.length();
+        int sections = (int) Math.ceil(n / (2 * numRows - 2.0));
+        int numCols = sections * (numRows - 1);
+        char[][] convert = new char[numRows][numCols];
+        for(char[] i: convert) Arrays.fill(i,' ');
+        int row = 0;
+        int col = 0;
+        int idx = 0;
+        while(idx < n) {
+            while(row < numRows && idx < n) {
+                convert[row][col] = s.charAt(idx++);
+                row++;
+            }
+            row -= 2;
+            col += 1;
+            while(row > 0 && col < numCols && idx < n) {
+                convert[row][col] = s.charAt(idx++);
+                row--;
+                col++;
+            }
+        }
+        for(char[] i: convert) {
+            for(char j: i) {
+                if(j != ' ') ans += j;
+            }
+        }
+        return ans;
+    }
+}
