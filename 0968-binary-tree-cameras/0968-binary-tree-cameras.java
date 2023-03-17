16d09addff6a979040d3132401f8eb6939b8c027
Prayas Gautam <prayas12gautam@gmail.com>
2023-03-17 09:45:35 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (17.48%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -15,24 +15,27 @@
  */
 class Solution {
     int count = 0;
+    final int covered = 1;
+    final int required = -1;
+    final int cameraFit = 2;
     public int minCameraCover(TreeNode root) {
         int c = CountCamera(root);
         if(c == -1) count++;
         return count;
     }
     public int CountCamera(TreeNode root) {
-        if(root == null) return 1;
+        if(root == null) return covered;
         int left = CountCamera(root.left);
         int right = CountCamera(root.right);
-        if(left == -1 || right == -1) {
+        if(left == required || right == required) {
             count += 1;
-            return 2;
+            return cameraFit;
         }
-        if(left == 1 && right == 1) return -1;
-        else return 1;
+        if(left == covered && right == covered) return required;
+        else return covered;
     }
     
     //1 -> Covered
     //-1 -> Required/ Not covered
-    //2 -> Fit resume
+    //2 -> Fit camera
 }
