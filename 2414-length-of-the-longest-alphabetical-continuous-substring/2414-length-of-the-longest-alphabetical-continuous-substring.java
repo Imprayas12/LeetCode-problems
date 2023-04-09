022843076dbcdb6ee1d8aad6116f60749d5086ab
Prayas Gautam <prayas12gautam@gmail.com>
2023-04-09 18:18:00 +0530

    Time: 14 ms (80.55%), Space: 42.6 MB (98.73%) - LeetHub

---
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestContinuousSubstring(String s) {
+        int start = 0;
+        int end = 1;
+        int ans = 1;
+        int n = s.length();
+        while(end < n) {
+            if(s.charAt(end) == s.charAt(end - 1) + 1) {
+                ans = Math.max(ans, end - start + 1);
+                end++;
+            }
+            else {
+                start = end;
+                end += 1;
+            }
+        }
+        return ans;
+    }
+}
