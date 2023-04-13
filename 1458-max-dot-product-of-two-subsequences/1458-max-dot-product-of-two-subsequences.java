6538981299d59d17556e1514bc6fd1d49b8bd2e9
Prayas Gautam <prayas12gautam@gmail.com>
2023-04-13 14:36:24 +0530

    Time: 10 ms (80.65%), Space: 42.5 MB (59.68%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,9 +1,6 @@
 class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length][nums2.length];
-        for(var i: dp) Arrays.fill(i, Integer.MIN_VALUE);
-        int ans = helper(nums1, nums2, 0, 0,false, dp);
-        return ans == -999999 ? -1 : ans;
+        return helperTB(nums1, nums2);
     }
     private int helper(int[] nums1, int[] nums2, int i, int j,boolean flag, int[][] dp) {
         if(i == nums1.length || j == nums2.length) {
@@ -15,5 +12,18 @@ class Solution {
         int t = nums1[i] * nums2[j] + take;
         int notTake = Math.max(helper(nums1, nums2, i + 1, j,false, dp), helper(nums1, nums2, i, j + 1, false, dp));
         return dp[i][j] = Math.max(nums1[i] * nums2[j], Math.max(t, notTake));
+    }
+    private int helperTB(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int spesal = Integer.MIN_VALUE;
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                spesal = Math.max(nums1[i - 1] * nums2[j - 1], spesal);
+                int take = nums1[i - 1] * nums2[j - 1] + dp[i - 1][j - 1];
+                int notTake = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = Math.max(nums1[i - 1] * nums2[j - 1], Math.max(take, notTake));
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1] == 0 ? spesal : dp[dp.length - 1][dp[0].length - 1];
     }
 }
