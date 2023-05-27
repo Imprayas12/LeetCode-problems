3d8e93748b4e2229fc096ff5fb0d70175ac0a3f8
Prayas Gautam <prayas12gautam@gmail.com>
2023-05-27 11:25:53 +0530

    Time: 15 ms (10.08%), Space: 44.3 MB (10.84%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,19 +1,18 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int[] left = new int[n];
-        left[0] = height[0];
-        for(int i = 1; i < n; i++) {
-            left[i] = Math.max(left[i - 1],height[i]);
-        }
-        int[] right = new int[n];
-        right[n - 1] = height[n - 1];
-        for(int i = n - 2; i >= 0; i--) {
-            right[i] = Math.max(right[i + 1],height[i]);
-        }
+        Stack<Integer> stack = new Stack<>();
         int ans = 0;
+        int n = height.length;
         for(int i = 0; i < n; i++) {
-            ans += Math.min(left[i],right[i]) - height[i];
+            while(!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int base = height[stack.peek()];
+                stack.pop();
+                if(stack.isEmpty()) break;
+                int width = i - stack.peek() - 1;
+                int len = Math.min(height[i], height[stack.peek()]) - base;
+                ans += len * width;
+            }
+            stack.push(i);
         }
         return ans;
     }
