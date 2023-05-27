2af84522d5e74c16593a6f95bc97cee745947e9f
Prayas Gautam <prayas12gautam@gmail.com>
2023-05-27 11:36:38 +0530

    Time: 2 ms (99.64%), Space: 55.4 MB (90.09%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,21 +1,17 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int index = 0;
-        int currGas = 0;
-        int remGas = 0;
-        for(int i = 0; i < gas.length ; i++){
-            currGas += gas[i]-cost[i]; 
-            if(currGas < 0){
-                index = i + 1;
-                remGas += currGas;
-                currGas = 0;
+        int start = 0;
+        int current_gas = 0;
+        int remaining_gas = 0;
+        int n = gas.length;
+        for(int i = 0; i < n; i++) {
+            current_gas += gas[i] - cost[i];
+            if(current_gas < 0) {
+                start = i + 1;
+                remaining_gas += current_gas;
+                current_gas = 0;
             }
         }
-        return (remGas + currGas >= 0) ? index : -1;
+        return remaining_gas + current_gas >= 0 ? start : -1;
     }
-} 
-
-/*
-Start from the first index and check if the amount of gas available at current station is enough to travel to next station or not. If the amount of gas available is not enough then it means that we cannot start from the index with which we started. hence we will change the index and then we'll set the current gas amount to zero and before setting it to zero we will add the difference in totalGas variable which will help us to decide if we can travel the whole array or not. If the totalGas amount is negative then it means that we cannot travel the whole array with the given sequence of gas stations and cost.
-
-*/
+}
