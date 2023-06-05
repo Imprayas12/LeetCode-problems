4d219ed6bd68abd47b1f0f19b8d81f12d9788d9f
Prayas Gautam <prayas12gautam@gmail.com>
2023-06-05 14:46:35 +0530

    Time: 6 ms (62.51%), Space: 44.6 MB (6.97%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -3,9 +3,9 @@ class Solution {
         int[] ans = new int[A.length];
         ans[0] = A[0] == B[0] ? 1 : 0;
         ans[A.length - 1] = A.length;
-        Set<Integer> setA = new TreeSet<>();
+        Set<Integer> setA = new HashSet<>();
         setA.add(A[0]);
-        Set<Integer> setB = new TreeSet<>();
+        Set<Integer> setB = new HashSet<>();
         setB.add(B[0]);
         for(int i = 1; i < A.length - 1; i++) {
             if(A[i] == B[i]) {
