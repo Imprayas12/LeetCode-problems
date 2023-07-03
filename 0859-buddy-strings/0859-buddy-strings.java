78efc3b2171fe330af7332672dde393b22891b94
Prayas Gautam <prayas12gautam@gmail.com>
2023-07-03 12:10:43 +0530

    Time: 2 ms (79.89%), Space: 41.4 MB (83.85%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        if(s.equals(goal)) {
+            int[] frequency = new int[26];
+            for (char ch : s.toCharArray()) {
+                frequency[ch - 'a'] += 1;
+                if (frequency[ch - 'a'] == 2) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        int n = s.length();
+        List<Integer> indices = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(s.charAt(i) != goal.charAt(i)) {
+                indices.add(i);
+            }
+        }
+        if(indices.size() != 2) return false;
+        int idx1 = indices.get(0);
+        int idx2 = indices.get(1);
+        if(s.charAt(idx1) == goal.charAt(idx2) && s.charAt(idx2) == goal.charAt(idx1)) return true;
+        return false;
+        
+    }
+}
