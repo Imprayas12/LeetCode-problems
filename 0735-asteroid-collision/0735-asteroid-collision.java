21961f074a4e4a3bca8c41c1bfcedae13485df2d
Prayas Gautam <prayas12gautam@gmail.com>
2023-07-20 17:44:49 +0530

    Time: 10 ms (73.15%), Space: 45.2 MB (6.36%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+    Stack<Integer> visited = new Stack<>();
+    for (int cur : asteroids) {
+      while (cur < 0 && !visited.isEmpty() && visited.peek() > 0) {
+        int top = visited.pop();
+        int left = top + cur;
+        if (left == 0) {
+          cur = 0;
+        } else if (left > 0) {
+          cur = top; // Pick the largest
+        } else {
+          // ; cur does not change
+        }
+      }
+
+      // If something is left
+      if (cur != 0) {
+        visited.push(cur);
+      }
+    }
+
+    int[] res = new int[visited.size()];
+    for (int i = res.length - 1; i >= 0; i--) {
+      res[i] = visited.pop();
+    }
+    return res;
+  }
+}
